5770e4c7b486da0b8880e43246981b34e5b267f5
parkoon <par_koon@naver.com>
2019-10-10 15:05:42 +0900

    Set: antd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./pages/Home";
+import "antd/dist/antd.css";
 
 ReactDOM.render(<Home />, document.getElementById("root"));
