1a41ae023bf5d88b6f8cb8b0ffdba75786a45c88
parkoon <par_koon@naver.com>
2019-10-11 17:34:12 +0900

    Styling: nav bar

--- src/components/navbar/NavMenu.js
+++ src/components/navbar/NavMenu.js
@@ -20,29 +20,46 @@ class NavMenu extends Component {
           theme="dark"
           inlineCollapsed={this.state.collapsed}
           style={{ background: "transparent" }}
+          className="menu"
         >
-          <Menu.Item key="1" onClick={() => console.log("메뉴 클릭")}>
+          <Menu.Item
+            key="1"
+            onClick={() => console.log("메뉴 클릭")}
+            className="menu__item"
+          >
             <Link to="/">
-              <Icon type="pie-chart" />
-              <span>대시보드</span>
+              <Icon className="item__icon" type="layout" theme="filled" />
+              <span className="item__title">대시보드</span>
             </Link>
           </Menu.Item>
-          <Menu.Item key="2" onClick={() => console.log("메뉴 클릭")}>
+          <Menu.Item
+            key="2"
+            onClick={() => console.log("메뉴 클릭")}
+            className="menu__item"
+          >
             <Link to="/users">
-              <Icon type="pie-chart" />
-              <span>회원 관리</span>
+              <Icon className="item__icon" type="contacts" theme="filled" />
+              <span className="item__title">회원 관리</span>
             </Link>
           </Menu.Item>
-          <Menu.Item key="3" onClick={() => console.log("메뉴 클릭")}>
+          <Menu.Item
+            key="3"
+            onClick={() => console.log("메뉴 클릭")}
+            className="menu__item"
+          >
             <Link to="/debates">
-              <Icon type="pie-chart" />
-              <span>토론방 관리</span>
+              <Icon className="item__icon" type="message" theme="filled" />
+              <span className="item__title">토론방 관리</span>
             </Link>
           </Menu.Item>
-          <Menu.Item key="4" onClick={() => console.log("메뉴 클릭")}>
+          <Menu.Item
+            key="4"
+            onClick={() => console.log("메뉴 클릭")}
+            className="menu__item"
+          >
             <Link to="qna">
-              <Icon type="pie-chart" />
-              <span>문의 게시판</span>
+              <Icon className="item__icon" type="table" />
+              <span className="item__title">문의 게시판</span>
             </Link>
           </Menu.Item>
         </Menu>
