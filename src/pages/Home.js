76702b6d891424f3a4003b5f892ace249d727c81
parkoon <par_koon@naver.com>
2019-10-11 14:23:08 +0900

    Styling: responsive dashboard (table)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -95,45 +95,51 @@ class Home extends Component {
               </Col>
             </Row>
           </div>
-          <Row style={{ marginBottom: "12px" }}>
-            <Col span={24}>
-              <LineGraph
-                title="라인 그래프"
-                dataLabel="사용자 유입량"
-                data={[65, 59, 80, 81, 56, 55, 40]}
-                xAxisLabels={[
-                  1,
-                  2,
-                  3,
-                  4,
-                  5,
-                  6,
-                  76,
-                  7,
-                  8,
-                  8,
-                  9,
-                  9,
-                  7,
-                  5,
-                  4,
-                  3,
-                  1,
-                  3,
-                  4,
-                  5
-                ]}
-              />
-            </Col>
-          </Row>
-          <Row gutter={8}>
-            <Col span={12}>
-              <UserTable users={users} />
-            </Col>
-            <Col span={12}>
-              <UserTable users={users} />
-            </Col>
-          </Row>
+
+          <div className="dashboard__graph-container">
+            <Row style={{ marginBottom: "12px" }}>
+              <Col span={24}>
+                <LineGraph
+                  title="라인 그래프"
+                  dataLabel="사용자 유입량"
+                  data={[65, 59, 80, 81, 56, 55, 40]}
+                  xAxisLabels={[
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6,
+                    76,
+                    7,
+                    8,
+                    8,
+                    9,
+                    9,
+                    7,
+                    5,
+                    4,
+                    3,
+                    1,
+                    3,
+                    4,
+                    5
+                  ]}
+                />
+              </Col>
+            </Row>
+          </div>
+
+          <div className="dashboard__boards-container">
+            <Row gutter={8}>
+              <Col lg={12} md={24} className="boards__col">
+                <UserTable users={users} />
+              </Col>
+              <Col lg={12} md={24} className="boards__col">
+                <UserTable users={users} />
+              </Col>
+            </Row>
+          </div>
         </Layout.Content>
       </div>
     );
