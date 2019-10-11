2c76522c94363102f67aedc61589937d455275d8
parkoon <par_koon@naver.com>
2019-10-11 15:57:51 +0900

    Styling: graph

--- src/components/graph/LineGraph.js
+++ src/components/graph/LineGraph.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Divider, Row, Col, Icon, Tooltip } from "antd";
 import Chart from "chart.js";
 import PropTypes from "prop-types";
+import colors from "../../constants/colors";
+import { NONAME } from "dns";
 
 class LineGraph extends React.Component {
   componentDidMount() {
     console.log(this.props);
     var ctx = document.getElementById("myChart");
+
+    // Chart.defaults.LineWithLine = Chart.defaults.line;
+    // Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+    //   draw: function(ease) {
+    //     Chart.controllers.line.prototype.draw.call(this, ease);
+    //     console.log(Date.now());
+    //     if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+    //       var activePoint = this.chart.tooltip._active[0],
+    //         ctx = this.chart.ctx,
+    //         x = activePoint.tooltipPosition().x,
+    //         topY = this.chart.scales["y-axis-0"].top,
+    //         bottomY = this.chart.scales["y-axis-0"].bottom;
+
+    //       // draw line
+    //       ctx.save();
+    //       ctx.beginPath();
+    //       ctx.moveTo(x, topY);
+    //       ctx.lineTo(x, bottomY);
+    //       ctx.lineWidth = 2;
+    //       ctx.strokeStyle = "#07C";
+    //       ctx.stroke();
+    //       ctx.restore();
+    //     }
+    //   }
+    // });
+    // https://stackoverflow.com/questions/45159895/moving-vertical-line-when-hovering-over-the-chart-using-chart-js
     new Chart(ctx, {
       type: "line",
       data: {
@@ -15,14 +43,54 @@ class LineGraph extends React.Component {
           {
             label: this.props.dataLabel,
             data: this.props.data,
-            fill: false,
-            borderColor: "rgb(75, 192, 192)",
-            lineTension: 0.1
+            fill: false, // 라인 안쪽을 채울지 여부
+            borderColor: "tomato", // 라인 색
+            lineTension: 0.1, // 라인 굴곡
+            pointBorderColor: "tomato",
+            pointBackgroundColor: "#fff",
+            pointHoverRadius: 4,
+            pointRadius: 4,
+            pointHoverBackgroundColor: "tomato"
           }
         ]
       },
       options: {
-        responsive: false
+        responsive: true,
+        legend: {
+          display: false
+        },
+        tooltips: {
+          intersect: false
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                fontSize: 10,
+                fontColor: "lightgrey"
+              }
+            }
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                drawBorder: false
+                // display: false,
+              },
+              ticks: {
+                beginAtZero: true,
+                // fontSize: 15
+                fontColor: "#fff",
+                maxTicksLimit: 5,
+                padding: 25
+              }
+            }
+          ]
+        }
+
         // maintainAspectRatio: false,
       }
     });
@@ -30,12 +98,35 @@ class LineGraph extends React.Component {
 
   render() {
     return (
-      <Card title={this.props.title} bordered={false}>
-        <canvas
-          id="myChart"
-          class="chartjs"
-          style={{ display: "block", width: "100%", height: "385px" }}
-        ></canvas>
+      <Card bordered={false} className="wrapper__graph">
+        {/* <Card title={this.props.title} bordered={false}> */}
+        <div className="graph__box">
+          <canvas
+            id="myChart"
+            class="chartjs"
+            style={{ display: "block", width: "100%", height: "385px" }}
+          ></canvas>
+        </div>
+
+        <Tooltip placement="bottom" title={"통계 새로고침"}>
+          <Icon type="reload" className="graph__reload" />
+        </Tooltip>
+
+        <Divider></Divider>
+        <Row className="graph__info">
+          <Col span={2} className="info__yesterday">
+            <h1 className="info__title">어제 방문수</h1>
+            <span className="info__data">1</span>
+          </Col>
+          <Col span={2} className="info__today">
+            <h1 className="info__title">오늘 방문수</h1>
+            <span className="info__data">1</span>
+          </Col>
+          <Col span={20} className="info__total">
+            <h1 className="info__title">누적 방문수</h1>
+            <span className="info__data">1</span>
+          </Col>
+        </Row>
       </Card>
     );
   }
