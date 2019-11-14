881e9716fa496c2e8b48e1640fc0078606a710bb
parkoon <par_koon@naver.com>
2019-11-14 09:57:30 +0900

    Updates

---
+++ precache-manifest.cd395cd27c00dd8e21969a1258d0aef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab0fa7b1da3eef94b9a5f6a427c2edd",
+    "url": "/works-debate-admin/index.html"
+  },
+  {
+    "revision": "198b1d60ee747010b0f3",
+    "url": "/works-debate-admin/static/css/2.50e3d594.chunk.css"
+  },
+  {
+    "revision": "4de0deb22d08ce3e7352",
+    "url": "/works-debate-admin/static/css/main.cb63f6f2.chunk.css"
+  },
+  {
+    "revision": "198b1d60ee747010b0f3",
+    "url": "/works-debate-admin/static/js/2.7d3b9af5.chunk.js"
+  },
+  {
+    "revision": "4de0deb22d08ce3e7352",
+    "url": "/works-debate-admin/static/js/main.f2272f6b.chunk.js"
+  },
+  {
+    "revision": "97d618b2b1225b6b7549",
+    "url": "/works-debate-admin/static/js/runtime-main.7941b178.js"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/works-debate-admin/static/media/sidebar-bg.d30c9e30.jpg"
+  }
+]);
